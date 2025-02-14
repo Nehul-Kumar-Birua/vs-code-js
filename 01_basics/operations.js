cda417468288206acd46482fc7093ec7d43c68c9
Nehul-Kumar-Birua <nehulk.birua@gmail.com>
2025-02-14 14:10:22 +0000

    done

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -23,10 +23,10 @@ console.log('1'+ 2 + 2);// 122
 console.log( 1 +'2'+ 2);  // 122
 console.log( 1 + 2 +'2'); // 32
 
-console.log((3+4) * 5 % 3);
+console.log((3+4) * 5 % 3); // 2
 
-console.log(+true);
-console.log(+'');
+console.log(+true);  // 1
+console.log(+'');   // 0
 
 let num1, num2, num3
 
@@ -35,6 +35,6 @@ num1 = num2 = num3 = 2 + 2
 let gameCounter = 100
 //gameCounter++
 ++gameCounter
-console.log(gameCounter);
+console.log(gameCounter); // 101
 
 
