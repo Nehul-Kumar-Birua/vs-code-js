ea85ee10bec3399ae0a2592afc7de325931ade88
Nehul-Kumar-Birua <nehulk.birua@gmail.com>
2025-02-17 15:25:12 +0000

    a

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,50 @@ if (true){
     let a = 10
     const b = 20
     // c = 30
-    console.log('Inner:',a);
+    // console.log('Inner:',a);
     
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one () {
+    const username = 'Hello'
+    function two(){
+        const website = 'youtube'
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    const username = 'Hello'
+    if (username == 'Hello') {
+        const website = ' youtube'
+        // console.log(username + website);
+    }
+    // console.log(website);   
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++++++++++++++intereting+++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addtwo(2))
+
+const addtwo = function(num){
+    return num + 2
+}
